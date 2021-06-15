8f5c72cb6fafc0f558c605bc4cfda2ee33120285
Zhilkata <marty30003@gmail.com>
2021-06-15 18:01:28 +0300

    Django - Basic Standard User Functionality

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -10,5 +10,11 @@ urlpatterns = [
     # Topics page
     path('topics/', views.topics, name='topics'),
     # Detail page for single topic
-    path('topics/<int:topic_id>/', views.topic, name='topic')
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
+    # Page for adding new topics
+    path('new_topic/', views.new_topic, name='new_topic'),
+    # Page for adding new entries
+    path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry'),
+    # Page for editing entry
+    path('edit_entry/<int:entry_id>/', views.edit_entry, name='edit_entry'),
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-from .models import Topic
+from .models import Topic, Entry
+from .forms import TopicForm, EntryForm
+
 
 def index(request):
     """The home page for the app."""
     return render(request, 'index.html')
+
 
 def topics(request):
     """Show all topics."""
@@ -12,9 +15,67 @@ def topics(request):
     context = {'topics': topics}
     return render(request, 'topics.html', context)
 
+
 def topic(request, topic_id):
     """Show a single topic and all its entries."""
     topic = Topic.objects.get(id=topic_id)
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'topic.html', context)
+
+
+def new_topic(request):
+    """Add a new topic."""
+    if request.method != 'POST':
+        # No data, make blank form.
+        form = TopicForm()
+    else:
+        # POST data available, processing.
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topics')
+
+    # Display a blank or invalid form.
+    context = {'form': form}
+    return render(request, 'new_topic.html', context)
+
+
+def new_entry(request, topic_id):
+    """Add a new entry for selected topic."""
+    topic = Topic.objects.get(id=topic_id)
+
+    if request.method != 'POST':
+        # No data, creating blank form.
+        form = EntryForm()
+    else:
+        # POST data available, processing.
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return redirect('learning_logs:topic', topic_id=topic_id)
+
+    # Display blank/invalid form.
+    context = {'topic': topic, 'form': form}
+    return render(request, 'new_entry.html', context)
+
+
+def edit_entry(request, entry_id):
+    """Edit an existing entry."""
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+
+    if request.method != 'POST':
+        # Initial request
+        form = EntryForm(instance=entry)
+    else:
+        # POST data submitted, processing
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topic', topic_id=topic.id)
+
+    context = {'entry': entry, 'topic': topic, 'form': form}
+    return render(request, 'edit_entry.html', context)
